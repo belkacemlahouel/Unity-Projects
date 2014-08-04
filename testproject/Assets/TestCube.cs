deb32c4bcff57ba9713e14240aac7a07229fc9cd
belkalah-mbp <belkacem.lahouel@gmail.com>
2014-08-04 08:36:14 +0200

    Premier essai avec Unity. Création d'une boîte qui change de couleur (RGB).

---
+++ testproject/Assets/TestCube.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestCube : MonoBehaviour
+{
+    void Update ()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            gameObject.renderer.material.color = Color.red;
+        }
+        if(Input.GetKeyDown(KeyCode.G))
+        {
+            gameObject.renderer.material.color = Color.green;
+        }
+        if(Input.GetKeyDown(KeyCode.B))
+        {
+            gameObject.renderer.material.color = Color.blue;
+        }
+    }
+}
