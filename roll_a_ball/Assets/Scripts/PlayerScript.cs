5384670bf00a900ef0d8da514c252af4573e1414
belkalah-mbp <belkacem.lahouel@gmail.com>
2014-08-05 16:21:46 +0200

    La boule bouge suivant les flèches directionelles

---
+++ roll_a_ball/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	public float speed = 1.0f;
+
+	// FixedUpdate is called just before physical calculation
+	public void FixedUpdate() {
+		float moveX = Input.GetAxis("Horizontal");
+		float moveZ = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(moveX, 0.0f, moveZ);
+		Vector3 tmp = movement * speed * Time.deltaTime;
+
+		// Debug.Log("Tmp: (" + tmp.x + ", " + tmp.y + ", " + tmp.z + ")");
+
+		rigidbody.AddForce(tmp);
+	}
+}
