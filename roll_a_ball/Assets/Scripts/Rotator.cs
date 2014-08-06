d0275575815a671e2c3a22168b853b38fd022e44
belkalah-mbp <belkacem.lahouel@gmail.com>
2014-08-06 12:23:26 +0200

    Les pickups tournent

---
+++ roll_a_ball/Assets/Scripts/Rotator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+	public Vector3 direction;
+	
+	void Update () {
+		transform.Rotate(direction * Time.deltaTime);
+	}
+}
