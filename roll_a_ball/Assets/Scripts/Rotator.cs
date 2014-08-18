229e4db6de62cad5bb661ebefd03662440c037da
belkalah-mbp <belkacem.lahouel@gmail.com>
2014-08-18 15:56:43 +0200

    Essai d'ajout d'un texte au dessus des boules > ECHEC.

--- roll_a_ball/Assets/Scripts/Rotator.cs
+++ roll_a_ball/Assets/Scripts/Rotator.cs
@@ -1,11 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+// Prefabs script
 public class Rotator : MonoBehaviour {
 
 	public Vector3 direction;
-	
-	void Update () {
+	private int number;
+	private static int MAX_NUMBER = 0;
+
+	public void Start() {
+		++MAX_NUMBER;
+		number = MAX_NUMBER;
+		
+		Debug.Log("NUMBER: " + number);
+	}
+
+	public void Update() {
 		transform.Rotate(direction * Time.deltaTime);
 	}
 }
