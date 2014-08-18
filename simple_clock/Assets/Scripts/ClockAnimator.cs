f02d638514709d16df06b525dbbde8ca2110a967
belkalah-mbp <belkacem.lahouel@gmail.com>
2014-08-18 20:00:50 +0200

    Ajout de la fonctionnalité analogique (deux versions). Ajout exécutable.

--- simple_clock/Assets/Scripts/ClockAnimator.cs
+++ simple_clock/Assets/Scripts/ClockAnimator.cs
@@ -9,12 +9,37 @@ public class ClockAnimator : MonoBehaviour {
 		degS = 360f/60f;
 
 	public Transform hours, minutes, seconds;
+	public Boolean analog;
 
 	public void Update() {
-		DateTime time = DateTime.Now;
+		if (analog) {
+			// Not the best solution, there is a better one using C# functions
 
-		hours.localRotation = Quaternion.Euler(0f, 0f, time.Hour * -degH);
-		minutes.localRotation = Quaternion.Euler(0f, 0f, time.Minute * -degM);
-		seconds.localRotation = Quaternion.Euler(0f, 0f, time.Second * -degS);
+			/*DateTime time = DateTime.Now;
+
+			float tmp = -degH * (time.Hour + time.Minute/60 + time.Second/3600);
+			hours.localRotation = Quaternion.Euler(0f, 0f, tmp);
+
+			tmp = -degM * (time.Minute + time.Second/60);
+			minutes.localRotation = Quaternion.Euler(0f, 0f, tmp);
+
+			tmp = -degS * (time.Second);
+			seconds.localRotation = Quaternion.Euler(0f, 0f, tmp);*/
+
+            TimeSpan timespan = DateTime.Now.TimeOfDay;
+
+            hours.localRotation =
+                Quaternion.Euler(0f, 0f,(float)timespan.TotalHours * -degH);
+            minutes.localRotation =
+                Quaternion.Euler(0f, 0f,(float)timespan.TotalMinutes * -degM);
+            seconds.localRotation =
+                Quaternion.Euler(0f, 0f,(float)timespan.TotalSeconds * -degS);
+		} else {
+			DateTime time = DateTime.Now;
+
+			hours.localRotation = Quaternion.Euler(0f, 0f, time.Hour * -degH);
+			minutes.localRotation = Quaternion.Euler(0f, 0f, time.Minute * -degM);
+			seconds.localRotation = Quaternion.Euler(0f, 0f, time.Second * -degS);
+		}
 	}
 }
