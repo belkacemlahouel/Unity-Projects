c9297370d841e433348f9b8d10904ec921ab8cca
belkalah-mbp <belkacem.lahouel@gmail.com>
2014-08-18 18:28:22 +0200

    Spinning cube

---
+++ spinning_cube/Assets/_Scripts/Spinning.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spinning : MonoBehaviour {
+
+	public float speed = 10f;
+
+	void LateUpdate () {
+		transform.Rotate(Vector3.up, speed*Time.deltaTime);
+	}
+}
