adffe885122322d50000ec64c7c4b586fb13e56e
belkalah-mbp <belkacem.lahouel@gmail.com>
2014-08-18 19:35:32 +0200

    Première version fonctionnelle, montre digitale seulement...

---
+++ simple_clock/Assets/Scripts/ClockAnimator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System;
+
+public class ClockAnimator : MonoBehaviour {
+
+	private const float
+		degH = 360f/12f,
+		degM = 360f/60f,
+		degS = 360f/60f;
+
+	public Transform hours, minutes, seconds;
+
+	public void Update() {
+		DateTime time = DateTime.Now;
+
+		hours.localRotation = Quaternion.Euler(0f, 0f, time.Hour * -degH);
+		minutes.localRotation = Quaternion.Euler(0f, 0f, time.Minute * -degM);
+		seconds.localRotation = Quaternion.Euler(0f, 0f, time.Second * -degS);
+	}
+}
