7267b1c37abb851b35fb25e96f44cab134bf5c55
belkalah-mbp <belkacem.lahouel@gmail.com>
2014-08-19 13:11:15 +0200

    Ajout d'une seconde planète. Simulation non à l'échelle : cercle/ellipse, lumières, couleurs, vitesses, tailles.

--- orbiting_planet/Assets/Scripts/Orbiting.cs
+++ orbiting_planet/Assets/Scripts/Orbiting.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Orbiting : MonoBehaviour {
 
-	public float speed = 10f;
+	public float speed = 50f;
 
 	void Update() {
 		transform.Rotate(Vector3.up * Time.deltaTime * speed);
