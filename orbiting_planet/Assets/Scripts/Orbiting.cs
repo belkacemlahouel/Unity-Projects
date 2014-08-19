ec0b147122a914c71ed8ff89fa0241f3418bd035
belkalah-mbp <belkacem.lahouel@gmail.com>
2014-08-19 12:41:48 +0200

    Sphère qui orbite suivant un cercle autour d'une lumière ponctuelle ~ Soleil

---
+++ orbiting_planet/Assets/Scripts/Orbiting.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Orbiting : MonoBehaviour {
+
+	public float speed = 10f;
+
+	void Update() {
+		transform.Rotate(Vector3.up * Time.deltaTime * speed);
+	}
+}
