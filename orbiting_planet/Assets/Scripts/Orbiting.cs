857fe2c4b6d569982e634356cde68ba99b8b0a48
belkalah-mbp <belkacem.lahouel@gmail.com>
2014-08-19 20:13:28 +0200

    Vitesses proportionnelles pour les quatre premières planètes.

--- orbiting_planet/Assets/Scripts/Orbiting.cs
+++ orbiting_planet/Assets/Scripts/Orbiting.cs
@@ -3,9 +3,19 @@ using System.Collections;
 
 public class Orbiting : MonoBehaviour {
 
-	public float speed = 50f;
+	public float daysPerRevolution;
 
-	void Update() {
+	private float speed;
+
+	public void Start() {
+		speed = 360/daysPerRevolution * 10;
+		// 360° for X days, then for 1 day we have 360/X°
+		// If we want to see what happends during 1 year
+		// in only 1 frame, we can speed that up.
+		// We need to multiply that result by 365?
+	}
+
+	public void Update() {
 		transform.Rotate(Vector3.up * Time.deltaTime * speed);
 	}
 }
