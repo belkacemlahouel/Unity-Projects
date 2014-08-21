4feede3d43bf840f2c70a576434230f7e801c279
belkalah-mbp <belkacem.lahouel@gmail.com>
2014-08-21 20:40:42 +0200

    Début d'implémentation d'un algo de dessin de cercle par Bresenham.

---
+++ orbiting_planet/Assets/Scripts/Orbiting3.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System;
+
+public class Orbiting3 : MonoBehaviour {
+
+	// Using Bresenham's circle tracing algorigthm
+
+	public float speed = 10f;
+
+	private float radius;
+	private float radius2; // radius square
+	private float x, y, m;
+
+	public void Start() {
+		radius = Math.Abs(transform.position.z);
+		radius2 = radius*radius;
+
+		x = 0;
+		y = radius;
+		m = 5 - 4*radius;
+	}
+
+	public void Update() {
+		if (x <= y) {
+			// ...
+		}
+	}
+
+	private void affectPosition() {
+		// ...
+	}
+}
