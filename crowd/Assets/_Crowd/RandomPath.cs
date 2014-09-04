5eb33d163e8f6ab53c361abfcc87438a30f57608
belkacemlahouel-inria <belkacem.lahouel@gmail.com>
2014-09-04 16:50:59 +0200

    Nouveau script pour chemins aléatoires

--- crowd/Assets/_Crowd/RandomPath.cs
+++ crowd/Assets/_Crowd/RandomPath.cs
@@ -3,29 +3,29 @@ using System.Collections;
 
 public class RandomPath : MonoBehaviour {
 
-	// ************* todo *************
-	// A pointer on the board to set the limit up properly
-	// A key to press in order to start the animation
-	// Set the timer to not change that much directions...
-	// More randomness when setting directions/speeds ?
-	// Everybody needs to keep walking forward for the moment...
-	// ********************************
+	/* ************* todo *************
+	 * A pointer on the board to set the limit up properly
+	 * A key to press in order to start the animation
+	 * Set the timer to not change that much directions...
+	 * More randomness when setting directions/speeds ?
+	 * Everybody needs to keep walking forward for the moment...
+	 * ********************************/
 
-	// ******* how does it work *******
-	// Chooses one point on the plane area 	/!\ TODO
-	// Goes left/right until the x-coordinate is reached
-	// It has to turn for a while... 		/!\ TODO
-	// Then it goes for-/backward until the z-coordinate is reached
-	//
-	// Finally, another target point is chosen
-	// ********************************
+	/* ******* how does it work *******
+	 * Chooses one point on the plane area 	/!\ TODO
+	 * Goes left/right until the x-coordinate is reached
+	 * It has to turn for a while... 		/!\ TODO
+	 * Then it goes for-/backward until the z-coordinate is reached
+	 *
+	 * Finally, another target point is chosen
+	 * ********************************/
 
-	private readonly float EPSILON = 0.5f;
+	private readonly float EPSILON = 0.1f;
 	private readonly float LIMIT = 5f;
 	private readonly float TIMER = 5f;
 	private float limit;
 	private float x, z;
-	private float direction, speed;
+	public float direction, speed;
 	private Animator animator;
 	private float timerDirection, timerSpeed;
 
@@ -35,25 +35,67 @@ public class RandomPath : MonoBehaviour {
 		timerSpeed = TIMER+1f;
 		animator = GetComponent<Animator>();
 		newTarget();
+		// computeDirectionSpeed();
 	}
 	
 	public void Update() {
 		if (areWeAlmostThere()) {
 			newTarget();
 		} else {
-			goThere();
-		}
+			goThere();		}
 	}
 
 	private void newTarget() {
 		x = Random.Range(-limit, limit);
 		z = Random.Range(-limit, limit);
+
+		Debug.Log("newTarget : " + x + ", " + z);
 	}
 
 	private bool areWeAlmostThere() {
-		return 	(x > transform.position.x+EPSILON || x < transform.position.x-EPSILON) &&
-				(z > transform.position.z+EPSILON || z < transform.position.z-EPSILON);
+		bool tmp =  (transform.position.x < x+EPSILON || transform.position.x > x-EPSILON) &&
+					(transform.position.z < z+EPSILON || transform.position.z > z-EPSILON);
+		Debug.Log("areWeAlmostThere : " + tmp);
+
+		return 	tmp;
 	}
+
+	/***
+	 * This system uses trigonometry to determine the values for speed and direction
+	 * The movement is still anarchic bacause random
+	 * @bugged
+
+	private void goThere() {
+		Debug.Log("goThere");
+
+		computeDirectionSpeed();
+
+		if (x != transform.position.x) {
+			animator.SetFloat("Speed", speed);
+		}
+
+		if (z != transform.position.z) {
+			animator.SetFloat("Direction", direction);
+		}
+	}
+
+	private void computeDirectionSpeed() {
+		float hypothenuse, oppose, adjacent;
+
+		adjacent = x - transform.position.x;
+		oppose = z - transform.position.z;
+
+		hypothenuse = (float) System.Math.Sqrt(adjacent*adjacent + oppose*oppose);
+
+		speed = adjacent/hypothenuse * 2f; // Be careful, it can be very small
+		direction = oppose/hypothenuse * 2f;
+	}
+
+	***/
+
+	/***
+	 * First kind of anarchic movement, using complete randomness
+	 ***/
 
 	private void goThere() {
 		if (x != transform.position.x && timerDirection > TIMER) {
---
+++ crowd/Assets/_Crowd/RandomPath0.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomPath0 : MonoBehaviour {
+
+	private readonly float EPSILON = 0.01f;
+	private readonly float TIMER = 3f;
+	private readonly float LIMIT = 5f;
+	private float x, z, timer, limit;
+	private Animator animator;
+
+	public void Start() {
+		timer = TIMER-EPSILON;
+		limit = LIMIT-EPSILON;
+
+		animator = GetComponent<Animator>();
+		animator.SetFloat("Speed", 1);
+	}
+	
+	public void Update() {
+		if (timer > TIMER) {
+			timer = 0;
+
+			float direction;
+			direction = Random.Range(-0.5f, 0.5f);
+
+			// todo: add a parameter to allow
+			// random numbers in [-1, -0.7] U [0.7, 1] (as an example)
+			// with a check up of the current direction...
+
+			animator.SetFloat("Direction", direction);
+		} else {
+			timer += Time.deltaTime;
+		}
+	}
+}
