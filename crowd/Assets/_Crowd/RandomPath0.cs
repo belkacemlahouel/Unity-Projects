da4448c828cb84cd23ae81f2301ef91352add03f
belkacemlahouel-inria <belkacem.lahouel@gmail.com>
2014-09-05 11:27:32 +0200

    Le Robot peut accelerer, dans l'animation au moins

--- crowd/Assets/_Crowd/RandomPath0.cs
+++ crowd/Assets/_Crowd/RandomPath0.cs
@@ -1,6 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/***
+ * This script makes the Robot move always forward
+ * after one time frame "TIMER" (approx.),
+ * it will change its direction.
+ * This script is done (for the moment)
+ * in order to make the Robot move naturally,
+ * so there is not any possibility for an odd
+ * move like turning around one point...
+ * And the choice of direction is completely random.
+ ***/
+
 public class RandomPath0 : MonoBehaviour {
 
 	private readonly float EPSILON = 0.01f;
@@ -8,6 +19,8 @@ public class RandomPath0 : MonoBehaviour {
 	private readonly float LIMIT = 5f;
 	private float x, z, timer, limit;
 	private Animator animator;
+
+	public float speed;
 
 	public void Start() {
 		timer = TIMER-EPSILON;
@@ -24,8 +37,8 @@ public class RandomPath0 : MonoBehaviour {
 			float direction;
 			direction = Random.Range(-0.5f, 0.5f);
 
-			// todo: add a parameter to allow
-			// random numbers in [-1, -0.7] U [0.7, 1] (as an example)
+			// improvement: add a parameter to allow
+			// e.g. random numbers in [-1, -0.7] U [0.7, 1]
 			// with a check up of the current direction...
 
 			animator.SetFloat("Direction", direction);
